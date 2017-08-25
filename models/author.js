306b287b4999500fd845282be865fc560ca8e072
copperdime <mrs.kelly@gmail.com>
2017-08-24 17:45:34 -0700

    Pushing in userProfile files, .html, .js, and updates to the post routes. Images as well.

--- models/author.js
+++ models/author.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
     // the user's profile pic as a url (a string)
     profile_pic: DataTypes.STRING,
     // the user's age (an integer)
-    age: DataTypes.STRING,
+    age: DataTypes.INTEGER,
     // the user's gender (a string)
     gender: DataTypes.STRING,
     // the user's ethnicity (a string)
@@ -17,15 +17,15 @@ module.exports = function(sequelize, DataTypes) {
     // the name of the user's bootcamp (a string)
     bootcamp_name: DataTypes.STRING,
     // the duration of the user's program (a integer)
-    date_range: DataTypes.STRING,
+    date_range: DataTypes.INTEGER,
     // the user's age (a string)
     location: DataTypes.STRING,
     // the user's employment status (a boolean)
-    employed: DataTypes.STRING,
+    employed: DataTypes.BOOLEAN,
     // the user's job title after bootcamp (a string)
     title: DataTypes.STRING,
     // the user's salary (an integer)
-    salary: DataTypes.STRING
+    salary: DataTypes.INTEGER
   });
 
   // Author.sync();
